11c2538bf45ef955614f6bb7ca3a74917cc5fca4
주민관 <jumingwan@jumingwan-ui-MacBookPro.local>
2021-09-27 23:48:52 +0900

    ready for delpoy

--- myapi/settings.py
+++ myapi/settings.py
@@ -22,7 +22,8 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/3.2/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = os.environ.get('DJANGO_SECRET_KEY','django-insecure-m8pyg@720)&=!y^j4sax$th(mbqn##ui@1ek5anwb)-v=)lo@l')
+# SECRET_KEY = os.environ.get('DJANGO_SECRET_KEY','django-insecure-m8pyg@720)&=!y^j4sax$th(mbqn##ui@1ek5anwb)-v=)lo@l')
+SECRET_KEY = os.environ.get('DJANGO_SECRET_KEY','m8pyg@720)&=!y^j4sax$th(mbqn##ui@1ek5anwb)-v=)lo@l')
 
 
 # SECURITY WARNING: don't run with debug turned on in production!
